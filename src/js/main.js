66f6d4e9c0cf7de3482fef3a42dbbcae157642fe
yelyzavetapiatkova <yelyzaveta.piatkova@studentfr.ch>
2026-03-23 12:14:45 +0100

    intergation 3d (il manques les textures)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
@@ -12,13 +13,24 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  45,
+  75,
   window.innerWidth / window.innerHeight,
   1,
   1000,
 );
 camera.position.set(4, 5, 11);
 camera.lookAt(0, 0, 0);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 0;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
 
 const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
 groundGeometry.rotateX(-Math.PI / 2);
@@ -31,12 +43,15 @@ const groundMaterial = new THREE.MeshStandardMaterial({
 const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
 scene.add(groundMesh);
 
-const spotLight = new THREE.SpotLight(0xffffff, 3, 100, 0.2, 0.5);
-spotLight.position.set(0, 25, 0);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const spotLight = new THREE.SpotLight(0xffffff, 2000, 100, 0.2, 0.5);
+spotLight.position.set(0, 30, 0);
 scene.add(spotLight);
 
-const loader = new GLTFLoader().setPath("src/ocean/");
-loader.load("scene.gltf", (gltf) => {
+const loader = new GLTFLoader().setPath("sea/");
+loader.load("water.gltf", (gltf) => {
   const mesh = gltf.scene;
   mesh.position.set(0, 1.05, -1);
   scene.add(mesh);
